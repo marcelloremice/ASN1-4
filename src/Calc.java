66216f513ad19ee5c50253b18a53c2ed62a328b7
marcelloremice <marcus.remice@outlook.com>
2025-02-26 09:09:54 -0500

    Calc.java

--- src/Calc.java
+++ src/Calc.java
@@ -1,32 +1,32 @@
 import java.util.Scanner;
 public class Calc {
-        private double Num1;
-        private double Num2;
+        private double num1;
+        private double num2;
 
 
         public double getNum1() {
-                return Num1;
+                return this.num1;
         }
         public void setNum1(double number1){
-                this.Num1 = number1;
+                this.num1 = number1;
         }
-        public double getNum2(){
-                return Num2;
-        }
+        public double getNum2(){return this.num2;}
         public void setNum2(double number2){
-                this.Num2 = number2;
+                this.num2 = number2;
         }
         public double add() {
-                return Num1+Num2;
-        }
+                return this.num1+ this.num2;}
         public double subtract() {
-                return Num1-Num2;
+                return this.num1- this.num2;
         }
         public double multiply() {
-                return Num1*Num2;
+                return this.num1* this.num2;
         }
         public double divide() {
-                return Num1/Num2;
+                return this.num1/ this.num2;
         }
 
+        public String toString(){
+                return this.num1 + "\n" + this.num2;
+        }
 }
